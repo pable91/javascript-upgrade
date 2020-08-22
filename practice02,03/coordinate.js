7ee4600130700d4b2889bebbfb6c3ebf6b28144a
KimYongKwon <dydrnjs518@gmail.com>
2020-08-23 00:57:10 +0900

    practice03
    - webAPI practice (scroll)

--- practice02,03/coordinate.js
+++ practice02,03/coordinate.js
@@ -1,5 +1,11 @@
 const arr = document.querySelectorAll("div");
 
+const b1 = document.querySelector(".button1");
+const b2 = document.querySelector(".button2");
+const b3 = document.querySelector(".button3");
+
+const speical = document.querySelector(".speical");
+
 function func(e) {
   let cx = e.clientX;
   let cy = e.clientY;
@@ -25,8 +31,24 @@ function func(e) {
   console.log(`page : ${px}, ${py}`);
 }
 
+function ScrollbyY() {
+  window.scrollBy(0, 100);
+  // window.scrollTo(0, window.scrollY + 100);
+}
+
+function ScrolltoY() {
+  window.scrollTo(0, 100);
+}
+
+function ScrolltoSpecial() {
+  speical.scrollIntoView();
+}
+
 function init() {
   window.addEventListener("click", func);
+  b1.addEventListener("click", ScrollbyY);
+  b2.addEventListener("click", ScrolltoY);
+  b3.addEventListener("click", ScrolltoSpecial);
 }
 
 init();
